4cb005a0032f6dbd3889c7946a273364768ffcca
TALLURI VIKAS <vikky@TALLURIs-MacBook-Air.local>
2026-05-29 09:52:53 +0530

    ec2_instance

---
+++ ec2.tf
@@ -0,0 +1,17 @@
+# main.tf
+
+# AWS Provider
+provider "aws" {
+  region = "ap-south-1"   # Mumbai region
+}
+
+# EC2 Instance
+resource "aws_instance" "my_ec2" {
+
+  ami           = "ami-0f58b397bc5c1f2e8"   # Amazon Linux 2 AMI
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
